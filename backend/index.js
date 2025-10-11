04b4cee11b3c4f0d392df535aad5fc534aea3c61
phuongwj <phamjulia9@gmail.com>
2025-10-11 01:13:11 -0300

    fix typo

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const express = require('express')
-const bodyParser = requier('body-parser')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3001
 
